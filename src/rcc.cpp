5624732455f5e62fe6a5241d274b3ff9fbace178
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2018-12-08 20:35:50 -0500

    Add RNG module

--- src/rcc.cpp
+++ src/rcc.cpp
@@ -6,34 +6,35 @@
 #include "rcc.h"
 
 void rcc::init() {
-  LL_FLASH_SetLatency(LL_FLASH_LATENCY_3);
-  LL_RCC_HSI_Enable();
+	LL_FLASH_SetLatency(LL_FLASH_LATENCY_3);
+	LL_RCC_HSI_Enable();
 
-   /* Wait till HSI is ready */
-  while(LL_RCC_HSI_IsReady() != 1)
-  {
-    
-  }
-  LL_RCC_HSI_SetCalibTrimming(16);
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_13, 195, LL_RCC_PLLP_DIV_2);
-  LL_RCC_PLL_Enable();
+	/* Wait till HSI is ready */
+	while(LL_RCC_HSI_IsReady() != 1)
+	{
 
-   /* Wait till PLL is ready */
-  while(LL_RCC_PLL_IsReady() != 1)
-  {
-    
-  }
-  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_4);
-  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_2);
-  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+	}
+	LL_RCC_HSI_SetCalibTrimming(16);
+	LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_13, 195, LL_RCC_PLLP_DIV_2);
+	LL_RCC_PLL_ConfigDomain_48M(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_13, 195, LL_RCC_PLLQ_DIV_5);
+	LL_RCC_PLL_Enable();
 
-   /* Wait till System clock is ready */
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
-  {
-  
-  }
-  LL_Init1msTick(120000000);
-  LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
-  LL_SetSystemCoreClock(120000000);
+	/* Wait till PLL is ready */
+	while(LL_RCC_PLL_IsReady() != 1)
+	{
+
+	}
+	LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+	LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_4);
+	LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_2);
+	LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+	/* Wait till System clock is ready */
+	while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+	{
+
+	}
+	LL_Init1msTick(120000000);
+	LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+	LL_SetSystemCoreClock(120000000);
 }
---
+++ src/rng.cpp
@@ -0,0 +1,23 @@
+#include "rng.h"
+
+#include "stm32f2xx.h"
+#include "stm32f2xx_ll_rng.h"
+#include "stm32f2xx_ll_bus.h"
+
+void rng::init() {
+	LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_RNG);
+
+	LL_RNG_Enable(RNG);
+}
+
+uint32_t rng::get() {
+	while (!LL_RNG_IsActiveFlag_DRDY(RNG)) {;}
+
+	if (LL_RNG_IsActiveFlag_CECS(RNG) || LL_RNG_IsActiveFlag_SECS(RNG)) {
+		LL_RNG_Disable(RNG);
+		LL_RNG_Enable(RNG);
+		return 0x4158f;  // chosen by dice roll certainly random...
+	}
+
+	return LL_RNG_ReadRandData32(RNG);
+}
