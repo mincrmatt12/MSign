1b1387360a9015ee3af859f5b523d9d51b7f50dd
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2018-12-13 09:58:08 -0500

    add interrupts to nvic

--- stm/src/nvic.cpp
+++ stm/src/nvic.cpp
@@ -7,9 +7,15 @@
 void nvic::init() {
 	NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
-	NVIC_SetPriority(DMA2_Stream5_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),5, 0));
+	NVIC_SetPriority(DMA2_Stream5_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),6, 0));
 	NVIC_EnableIRQ(DMA2_Stream5_IRQn);
 
-	NVIC_SetPriority(TIM1_BRK_TIM9_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),4, 0));
+	NVIC_SetPriority(TIM1_BRK_TIM9_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),5, 0));
 	NVIC_EnableIRQ(TIM1_BRK_TIM9_IRQn);
+
+	NVIC_SetPriority(DMA1_Stream5_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),4, 0));
+	NVIC_EnableIRQ(DMA1_Stream5_IRQn);
+
+	NVIC_SetPriority(DMA1_Stream6_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),6, 0));
+	NVIC_EnableIRQ(DMA1_Stream6_IRQn);
 }
