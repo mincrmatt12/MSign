5471b12c304434ec14905dea4b132731394cf407
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2018-12-26 15:45:21 -0500

    Change plot to other method

---
+++ get_sizes.py
@@ -0,0 +1,13 @@
+import subprocess
+
+fs = subprocess.run(["arm-none-eabi-size", "stm/.pioenvs/nucleo_f207zg/firmware.elf", "esp/.pioenvs/nodemcuv2/firmware.elf"], stdout=subprocess.PIPE, encoding="utf-8").stdout.split("\n")[1:]
+
+sections = [[int(x) for x in y.split("\t")[:3]] for y in fs[:-1]]
+
+with open("stm.csv", "w") as f:
+    f.write('"flash","ram"\n')
+    f.write("{},{}".format(sections[0][0] + sections[0][1], sections[0][1] + sections[0][2]))
+
+with open("esp.csv", "w") as f:
+    f.write('"flash","ram"\n')
+    f.write("{},{}".format(sections[1][0] + sections[1][1], sections[1][1] + sections[1][2]))
