1e40c3e956febe4bff40de95c74f145b41e58e02
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2019-01-18 14:14:39 -0500

    Use home dir

--- get_sizes.py
+++ get_sizes.py
@@ -1,6 +1,6 @@
 import os
 
-fs = os.popen("arm-none-eabi-size stm/.pioenvs/nucleo_f207zg/firmware.elf esp/.pioenvs/nodemcuv2/firmware.elf").read().split("\n")[1:]
+fs = os.popen("{} stm/.pioenvs/nucleo_f207zg/firmware.elf esp/.pioenvs/nodemcuv2/firmware.elf".format(os.path.expanduser("~/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-size"))).read().split("\n")[1:]
 
 sections = [[int(x) for x in y.split("\t")[:3]] for y in fs[:-1]]
 
