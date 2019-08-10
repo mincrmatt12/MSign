0813340729c1410306487feeeed19f9a4ff9eed8
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2019-08-10 13:21:35 -0400

    begin new http client

--- esp/src/util.h
+++ esp/src/util.h
@@ -12,6 +12,6 @@ namespace util {
 	// Mallocs a buffer, and downloads.
 	// Also occasionally runs the serial buffer.
 	// Does not make a c_str, returns the length in a struct however. The response may be less than length if error is set.
-	Download download_from(const char * host, const char * path, uint32_t timeout=500);
-	Download download_from(const char * host, const char * path, const char * const headers[][2], uint32_t timeout=500);
+	Download download_from(const char * host, const char * path);
+	Download download_from(const char * host, const char * path, const char * const headers[][2]);
 }
