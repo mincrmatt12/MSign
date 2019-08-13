4d2e0f639b4bc6215bf43b7eef47aee4cf8d72b0
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2019-08-12 22:06:29 -0400

    generate sd script

--- espweb/generate_sd.py
+++ espweb/generate_sd.py
@@ -1,5 +1,55 @@
 """
 This script does two things, the first is running webpack to generate the pack.js, css and html files.
 Then it creates an archive file containing the ca certificates. It takes all the certs in the cert folder, converts them to der if neccesary and creates the cacert.ar file.
-The final step is it creates a zip archive which should be extracted to the root of the SD card
+The SD card structure is then located in the build folder
 """
+
+import os
+import shutil
+from subprocess import Popen, PIPE, call
+
+def create_directory():
+    if not os.path.exists("build"):
+        os.mkdir("build")
+    if not os.path.exists("build/ca"):
+        os.mkdir("build/ca")
+    if os.path.exists("build/web"):
+        shutil.rmtree("build/web")
+
+
+def copy_all_certificates():
+    res = []
+
+    for x in os.listdir("cert"):
+        if os.path.splitext(x)[1] == "der":
+            shutil.copy(os.path.join("cert", x), os.path.join("build", x))
+            res.append(os.path.join("build", x))
+        else:
+            newname = os.path.join("build", os.path.splitext(x)[0] + ".der")
+            ssl = Popen(['openssl','x509','-inform','PEM','-outform','DER','-out', newname], shell = False, stdin = PIPE)
+            pipe = ssl.stdin
+            pipe.write(thisPem)
+            pipe.close()
+            ssl.wait()
+            res.append(newname)
+    
+    return res
+
+
+def create_archive(files):
+    cmd = ['ar', 'q', 'build/ca/cacert.ar'] + files
+    call(cmd)
+
+    for i in files:
+        os.unlink(i)
+
+
+def create_page_data():
+    call(['yarn', 'build'])
+    shutil.copytree("web", "build/web")
+
+
+if __name__ == "__main__":
+    create_directory()
+    create_archive(copy_all_certificates())
+    create_page_data()
