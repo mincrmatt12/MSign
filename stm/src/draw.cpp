8e2a34b607497069e4591502aa528c05cdf74aa5
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2019-08-29 00:41:40 -0400

    port improved kerning tracker

--- stm/src/draw.cpp
+++ stm/src/draw.cpp
@@ -8,12 +8,12 @@ namespace draw {
 
 	int16_t search_kern_table(uint8_t a, uint8_t b, const int16_t * kern, const uint32_t size) {
 		uint32_t start = 0, end = size;
-		uint16_t needle = ((uint16_t)a + ((uint16_t)b << 8));
+		uint16_t needle = (((uint16_t)a << 8) + (uint16_t)b);
 		while (start != end) {
 			uint32_t head = (start + end) / 2;
-			uint16_t current = (uint16_t)(*(kern + head*3)) + (((uint16_t)(*(kern + head*3 + 1))) << 8);
+			uint16_t current = ((uint16_t)(kern[head*3]) << 8) + (uint16_t)(kern[head*3 + 1]);
 			if (current == needle) {
-				return *(kern + head*3 + 2);
+				return kern[head*3 + 2];
 			}
 			else {
 				if (start - end == 1 || end - start == 1) {
