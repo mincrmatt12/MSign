29c6f8ef350e025646a886b9ddd3d242574ed033
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2019-10-04 20:46:44 -0400

    antiflicker constants

--- stm/src/matrix.h
+++ stm/src/matrix.h
@@ -200,12 +200,12 @@ namespace led {
 				// Setup the timer.
 				LL_TIM_InitTypeDef tim_init = {0};
 
-				tim_init.Prescaler  = 7; // Set the timer to run at around 6Mhz, since it's stupid to do it any faster
+				tim_init.Prescaler  = 6; // Set the timer to run at around 6Mhz, since it's stupid to do it any faster
 				tim_init.Autoreload = 1;
 				tim_init.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
 				LL_TIM_Init(TIM1, &tim_init);
 
-				tim_init.Prescaler  = 31;
+				tim_init.Prescaler  = 28;
 				tim_init.Autoreload = 1;
 				tim_init.ClockDivision = LL_TIM_CLOCKDIVISION_DIV4;
 				LL_TIM_Init(TIM9, &tim_init);
