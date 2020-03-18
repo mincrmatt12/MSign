de3dd0fb2ff006689d468306b5df827dd40bc45c
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-03-17 22:54:14 -0400

    add alertstr skeleton

--- stm/src/tasks/ttc.h
+++ stm/src/tasks/ttc.h
@@ -2,6 +2,7 @@
 #define TTC_H
 
 #include "schedef.h"
+#include "vstr.h"
 #include <stdint.h>
 
 namespace tasks {
@@ -14,11 +15,13 @@ namespace tasks {
 	private:
 		void draw_bus();
 		bool draw_slot(uint16_t y, const uint8_t * name, uint64_t times[4], bool alert, bool delay);
+		void draw_alertstr();
 
 		uint8_t s_info = 0xff;
 		uint8_t s_t[3] = {0xff};
 		uint8_t s_tb[3] = {0xff};
 		uint8_t s_n[3] = {0xff};
+		srv::vstr::SizeVSWrapper<char, 256> s_alert;
 
 		bool ready;
 
