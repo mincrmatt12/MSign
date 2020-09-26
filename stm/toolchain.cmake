70a4b16aa466d2ba4c337c8e7ebd401d41ce81ce
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-09-25 21:23:47 -0400

    Change toolchain.cmake for STM to use find_program

--- stm/toolchain.cmake
+++ stm/toolchain.cmake
@@ -1,22 +1,13 @@
 # TOOLCHAIN SETUP
-set(TOOLCHAIN_DIR /usr/local/arm-none-eabi)
-if (NOT EXISTS ${TOOLCHAIN_DIR})
-	message(WARNING "Trying to use PIO toolchain as backup; please install a new version of gccarmnoneeabi for better debug info into /usr/local")
-	set(TOOLCHAIN_DIR $ENV{HOME}/.platformio/packages/toolchain-gccarmnoneeabi)
-	if (NOT EXISTS ${TOOLCHAIN_DIR})
-		message(FATAL_ERROR "Please build the bootloader first with platformio, as we steal it's dependencies")
-	else()
-		message(STATUS "Found toolchain at ${TOOLCHAIN_DIR}")
-	endif()
-else()
-	set(TOOLCHAIN_DIR /usr/local)
-endif()
+find_program(C_COMPILER arm-none-eabi-gcc)
+get_filename_component(TOOLCHAIN_DIR ${C_COMPILER} DIRECTORY)
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
-set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/bin/arm-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/bin/arm-none-eabi-g++)
-set(CMAKE_ASM_COMPILER ${TOOLCHAIN_DIR}/bin/arm-none-eabi-gcc)
+set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_DIR}/arm-none-eabi-gcc)
+set(OBJCOPY_EXECUTABLE ${TOOLCHAIN_DIR}/arm-none-eabi-objcopy)
 
 # make cmake build check happy
 set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs")
