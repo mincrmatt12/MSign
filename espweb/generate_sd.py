ce8d134ffecc582a5d5a1d9a03737dac5c1ab933
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-09-27 19:34:23 -0400

    Revert "Remove root CAs"
    This reverts commit 809599561bb93c596737ff93f49a0ad037f5c3cc.

--- espweb/generate_sd.py
+++ espweb/generate_sd.py
@@ -11,8 +11,41 @@ from subprocess import Popen, PIPE, call
 def create_directory():
     if not os.path.exists("build"):
         os.mkdir("build")
+    if os.path.exists("build/ca"):
+        shutil.rmtree("build/ca")
+    os.mkdir("build/ca")
     if os.path.exists("build/web"):
         shutil.rmtree("build/web")
+
+
+def copy_all_certificates():
+    res = []
+
+    for x in os.listdir("cert"):
+        if os.path.splitext(x)[1] == ".der":
+            shutil.copy(os.path.join("cert", x), os.path.join("build", x))
+            res.append(os.path.join("build", x))
+        else:
+            newname = os.path.join("build", os.path.splitext(x)[0] + ".der")
+            ssl = Popen(['openssl','x509','-inform','PEM','-outform','DER','-out', newname], shell = False, stdin = PIPE)
+            pipe = ssl.stdin
+            with open(os.path.join("cert", x)) as f:
+                pipe.write(f.read())
+            pipe.close()
+            ssl.wait()
+            res.append(newname)
+    
+    return res
+
+
+def create_archive(files):
+    print(files)
+    cmd = ['ar', 'q', 'build/ca/cacert.ar'] + files
+    print(cmd)
+    call(cmd)
+
+    for i in files:
+        os.unlink(i)
 
 
 def create_page_data():
@@ -23,4 +56,5 @@ def create_page_data():
 
 if __name__ == "__main__":
     create_directory()
+    create_archive(copy_all_certificates())
     create_page_data()
