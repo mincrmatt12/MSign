952c398ab5d08ff6dec38274111fcce7a5aaabcd
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-10-14 23:43:18 -0400

    Add more fakes in sim

--- sim/src/esp/esp_system.h
+++ sim/src/esp/esp_system.h
@@ -2,12 +2,16 @@
 #define ESP_SYSTEM_FAKE
 
 #define heap_caps_get_free_size(x) 10000
+#define esp_get_free_heap_size() 10000
 
 #include <stdlib.h>
+#include <stddef.h>
 
 static void esp_restart() {
 	exit(1);
 }
+
+static void esp_fill_random(void *buf, size_t len) {}
 
 #define esp_random random
 #define IRAM_ATTR
