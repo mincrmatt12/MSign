e55a8554b6c400f8289fc521947240ca7b097e29
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-10-16 04:06:15 -0400

    Change models slower

--- esp/main/grabber/modelserve.h
+++ esp/main/grabber/modelserve.h
@@ -7,7 +7,8 @@ namespace modelserve {
 	void init();
 	bool loop();
 
-	constexpr static auto modelserve_grabber = grabber::make_http_grabber(init, loop, pdMS_TO_TICKS(100*1000));
+	// Every 200 seconds (or 3m20 seconds) we change the model
+	constexpr static auto modelserve_grabber = grabber::make_http_grabber(init, loop, pdMS_TO_TICKS(200*1000));
 }
 
 #endif
