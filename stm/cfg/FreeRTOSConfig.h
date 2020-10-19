f8eddfdb69cc5117ab1f00d38666791c20936231
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-10-19 00:36:27 -0400

    Fix newlib reent pointer setup

--- stm/cfg/FreeRTOSConfig.h
+++ stm/cfg/FreeRTOSConfig.h
@@ -12,7 +12,7 @@ extern uint32_t SystemCoreClock;
 #define configCPU_CLOCK_HZ                (SystemCoreClock)
 #define configTICK_RATE_HZ                ((TickType_t)1000)
 #define configMINIMAL_STACK_SIZE          ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE             ((size_t)(11 * 1024))
+#define configTOTAL_HEAP_SIZE             ((size_t)(13 * 1024))
 #define configMAX_TASK_NAME_LEN           (8)
 #define configUSE_TRACE_FACILITY          0
 #define configUSE_16_BIT_TICKS            0
@@ -25,6 +25,7 @@ extern uint32_t SystemCoreClock;
 #define configUSE_APPLICATION_TASK_TAG    0
 #define configUSE_COUNTING_SEMAPHORES     1
 #define configGENERATE_RUN_TIME_STATS     0
+#define configUSE_NEWLIB_REENTRANT        1
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES           0
@@ -64,7 +65,7 @@ function. */
 routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
 INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
 PRIORITY THAN THIS! (higher priorities are lower numeric values. */
-#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 5
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 3
 
 /* Interrupt priorities used by the kernel port layer itself.  These are generic
 to all Cortex-M ports, and do not rely on any particular library functions. */
