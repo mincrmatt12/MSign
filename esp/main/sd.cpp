28c096294d80758850f01b8bf1accaa6c660774c
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2021-04-22 13:14:58 -0400

    re-add verbose prints in sd

--- esp/main/sd.cpp
+++ esp/main/sd.cpp
@@ -1008,7 +1008,7 @@ extern "C" DRESULT disk_read(BYTE, BYTE* buff, LBA_t sector, UINT count) {
 	portEXIT_CRITICAL();
 retry:
 	auto x = sd::read(buff, sector, count);
-	//ESP_LOGV(TAG, "read (%d/3) %d %d --> %d", tries, sector, count, (int)x);
+	ESP_LOGV(TAG, "read (%d/3) %d %d --> %d", tries, sector, count, (int)x);
 	if (x != sd::read_status::Ok) {
 		++tries;
 		if (tries < 3) goto retry;
@@ -1038,7 +1038,7 @@ extern "C" DRESULT disk_write(BYTE, const BYTE* buff, LBA_t sector, UINT count) 
 	portEXIT_CRITICAL();
 retry:
 	auto x = sd::write(buff, sector, count);
-	//ESP_LOGV(TAG, "write (%d/3) %d %d --> %d", tries, sector, count, (int)x);
+	ESP_LOGV(TAG, "write (%d/3) %d %d --> %d", tries, sector, count, (int)x);
 	if (x != sd::write_status::Ok) {
 		++tries;
 		if (tries < 3) goto retry;
