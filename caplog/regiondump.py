33c5e156f9127c0073ef9fba28a1acd70f6ba760
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2021-07-13 04:12:42 -0400

    update regiondump for new constants + handle invalid slotids better

--- caplog/regiondump.py
+++ caplog/regiondump.py
@@ -11,10 +11,10 @@ import slotlib
 # use gdb to generate a report
 
 tempstrs = {
-    1: "cold",
+    1: "coldwarm",
     2: "warm",
     3: "hot",
-    0: "undefined"
+    0: "cold"
 }
 
 locstrs = {
@@ -26,7 +26,7 @@ locstrs = {
 
 flagstrs = {
     1: "dirty",
-    2: "flush"
+    2: "unused"
 }
 
 class RawBHeapDump(gdb.Command):
@@ -85,6 +85,7 @@ class RawBHeapDump(gdb.Command):
             ) if x)
 
             blkdat = {
+                "raw": hdr,
                 "slotid": slotid,
                 "datasize": datasize,
                 "temperature": tempstrs[temperature] if temperature else None,
@@ -117,7 +118,7 @@ class RawBHeapDump(gdb.Command):
                 else:
                     act_off = slotoffdb[slotid]
                     slotoffdb[slotid] += datasize
-                blkdat["slotname"] = slotlib.slot_types[slotid][0]
+                blkdat["slotname"] = slotlib.slot_types[slotid][0] if slotid in slotlib.slot_types else "<unk>"
                 blkdat["slotoffset"] = act_off
             record.append(blkdat)
 
@@ -138,51 +139,46 @@ class BHeapDump(gdb.Command):
         region_blob = gdb.parse_and_eval(args[0])
         fname = args[1]
 
-        print("got blob ", region_blob, region_blob.address, region_blob.type.sizeof)
-        
         memarr = gdb.selected_inferior().read_memory(region_blob.address, region_blob.type.sizeof)
-
-        print("got memarr", memarr)
 
         pos = 0
 
         slotoffdb = {}
 
-        with open(fname, "w") as f:
-            while pos < region_blob.type.sizeof:
-                hdr = struct.unpack_from("<I", memarr, pos)[0]
-                slotid = hdr & 0b111111111111
-                datasize = (hdr >> 12) & 0b11111111111111
-                temperature = (hdr >> (12+14)) & 0b11
-                location = (hdr >> (12+14+2)) & 0b11
-                flags = (hdr >> (12+14+2+2)) & 0b11
-                pos += 4
+        while pos < region_blob.type.sizeof:
+            hdr = struct.unpack_from("<I", memarr, pos)[0]
+            slotid = hdr & 0b111111111111
+            datasize = (hdr >> 12) & 0b11111111111111
+            temperature = (hdr >> (12+14)) & 0b11
+            location = (hdr >> (12+14+2)) & 0b11
+            flags = (hdr >> (12+14+2+2)) & 0b11
+            pos += 4
 
-                rounded_datasize = datasize if datasize % 4 == 0 else datasize + (4 - (datasize % 4))
-                flagstr = ", ".join(flagstrs[x] for x in (
-                    flags & 1, flags & 2
-                ) if x)
-                    
-                print(f"- <{slotid:03x}> / {datasize} / {tempstrs[temperature]} / {locstrs[location]} / ({flagstr})", file=f)
-
-                slotcontents_pos = pos
-                if location != 3:
-                    pos += rounded_datasize
+            rounded_datasize = datasize if datasize % 4 == 0 else datasize + (4 - (datasize % 4))
+            flagstr = ", ".join(flagstrs[x] for x in (
+                flags & 1, flags & 2
+            ) if x)
                 
-                if slotid == 4081:
-                    print("  End", file=f)
-                    break
-                elif slotid == 4080:
-                    print(f"  Empty", file=f)
-                    continue
+            print(f"- <{slotid:03x}> / {datasize} / {tempstrs[temperature]} / {locstrs[location]} / ({flagstr})")
+
+            slotcontents_pos = pos
+            if location != 3:
+                pos += rounded_datasize
+            
+            if slotid == 4081:
+                print("  End")
+                break
+            elif slotid == 4080:
+                print(f"  Empty")
+                continue
+            else:
+                if slotid not in slotoffdb:
+                    slotoffdb[slotid] = datasize
+                    act_off = 0
                 else:
-                    if slotid not in slotoffdb:
-                        slotoffdb[slotid] = datasize
-                        act_off = 0
-                    else:
-                        act_off = slotoffdb[slotid]
-                        slotoffdb[slotid] += datasize
-                    print(f"  {slotlib.slot_types[slotid][0]} @ {act_off:03x}", file=f)
+                    act_off = slotoffdb[slotid]
+                    slotoffdb[slotid] += datasize
+                print(f"  {slotlib.slot_types[slotid][0] if slotid in slotlib.slot_types else '<unrecognized>'} @ {act_off:03x}")
 
 BHeapDump()        
 RawBHeapDump()
