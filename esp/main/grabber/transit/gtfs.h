5af5e8d44a79fa8d038816a85873b6b47302f6aa
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2021-08-12 02:55:39 -0400

    slow down gtfs

--- esp/main/grabber/transit/gtfs.h
+++ esp/main/grabber/transit/gtfs.h
@@ -8,7 +8,7 @@ namespace transit::gtfs {
 	bool loop();
 
 	// Update every minute
-	constexpr static auto gtfs_grabber = grabber::make_https_grabber(init, loop, pdMS_TO_TICKS(60*1000));
+	constexpr static auto gtfs_grabber = grabber::make_https_grabber(init, loop, pdMS_TO_TICKS(90*1000));
 }
 
 #endif
