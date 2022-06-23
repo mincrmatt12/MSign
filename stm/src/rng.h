5adb78be01b60577c9208e2d0eb7e11e442e09c0
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2022-06-23 03:47:19 -0400

    Add STL-compatible random engine

--- stm/src/rng.h
+++ stm/src/rng.h
@@ -2,11 +2,23 @@
 #define RNG_H
 
 #include <stdint.h>
+#include <type_traits>
 
 namespace rng {
 	void init();
 	uint32_t get();
 	uint16_t getclr();
+
+	struct randengine {
+		using result_type = uint32_t;
+
+		constexpr inline result_type min() const { return 0; }
+		constexpr inline result_type max() const { return INT32_MAX; }
+		
+		result_type operator()() {
+			return get();
+		}
+	};
 }
 
 #endif
