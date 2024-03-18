48b87ebb201587cb49c05bdad770828088529f0b
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2024-03-17 21:08:08 -0400

    Add crashlog component
    Uses uninitialized memory to share a crash log with the bheap

---
+++ esp/components/crashlogs/include/crashlog_storage.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <utility>
+
+namespace crashlogs {
+	// Stores crash logs or a normal type. The normal type is not destructed when the crashlogs run, so this should only be used
+	// for some infinite-lifetime global.
+	//
+	// Objects of this type _must_ be declared _NOINIT for this to work across reboots properly.
+	template<typename Inner>
+	struct CrashBuffer {
+		CrashBuffer() {}
+
+		// If there is a saved crash log, this returns it.
+		const char * saved_log() const {
+			if (log_data)
+				return log_data.data.start_of_message;
+			else
+				return nullptr;
+		}
+
+		size_t saved_log_length() const {
+			if (log_data)
+				return log_data.data.length_of_message;
+			else
+				return 0;
+		}
+
+		// Destroys the crash log (if any exists) and initializes the Inner type.
+		void init_underlying() {
+			memset(underlying_raw, 0, sizeof underlying_raw);
+			new (&underlying) Inner{};
+		}
+
+		// Silently drops the Inner type and begins writing crash data into RAM.
+		void start_log() {
+			memset(free_message_space, 0, sizeof free_message_space);
+			log_data.data = typename LogHeader::LogHeaderData(free_message_space, 0);
+		}
+
+		enum WriteStatus {
+			WriteOk,
+			WriteBufferFull,
+			WriteNotStarted
+		};
+
+		// Writes data into the crash log, returning a status code about whether it failed.
+		WriteStatus write_log(const char *data, size_t length) {
+			if (!log_data)
+				return WriteNotStarted;
+			size_t available_space = max_message_size - log_data.data.length_of_message;
+			size_t new_length = length > available_space ? available_space : length;
+			memmove(free_message_space + log_data.data.length_of_message, data, new_length);
+			log_data.data = typename LogHeader::LogHeaderData(free_message_space, log_data.data.length_of_message + new_length);
+			if (new_length < length)
+				return WriteBufferFull;
+			else
+				return WriteOk;
+		}
+
+		// Returns a region of memory that can be used safely for constructing log messages.
+		std::pair<char *, size_t> log_scratch_buffer() const {
+			if (!log_data)
+				return {nullptr, 0};
+
+			return std::make_pair(
+					free_message_space + log_data.data.length_of_message,
+					max_message_size - log_data.data.length_of_message
+			);
+		}
+
+		auto& operator*() { return underlying; }
+		const auto& operator*() const { return underlying; }
+
+		auto* operator->() { return &underlying; }
+		const auto* operator->() const { return &underlying; }
+	private:
+		union LogHeader {
+			intptr_t raw_data[3];
+			struct LogHeaderData {
+				const char * start_of_message;
+				size_t       length_of_message;
+				intptr_t     checksum;
+
+				LogHeaderData(const char *som, size_t lom) :
+					start_of_message(som),
+					length_of_message(lom),
+					checksum((intptr_t)0x0badc4a45 - (intptr_t)som - (intptr_t)lom) {}
+			} data;
+
+			operator bool() const {
+				return raw_data[0] + raw_data[1] + raw_data[2] == 0x0badc4a45;
+			}
+		};
+
+		const static inline size_t max_message_size = sizeof(Inner) - sizeof(LogHeader);
+
+		union {
+			struct {
+				LogHeader log_data;
+				mutable char free_message_space[max_message_size];
+			};
+			Inner underlying;
+			alignas(Inner) char underlying_raw[sizeof(Inner)];
+		};
+	};
+}
---
+++ esp/components/crashlogs/include/crashlog_writer.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "crashlog_storage.h"
+#include <esp8266/backtrace.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <freertos/xtensa_context.h>
+#include <sdkconfig.h>
+#include <esp8266/eagle_soc.h>
+
+namespace crashlogs {
+	template<typename InnerIgnored>
+	void write_panic_frame(CrashBuffer<InnerIgnored>& buffer, void *frame, int wdt) {
+		buffer.start_log();
+
+		XtExcFrame *exc_frame = static_cast<XtExcFrame *>(frame);
+		
+		static const char *sdesc[] = {
+			"PC",   "PS",   "A0",   "A1",
+			"A2",   "A3",   "A4",   "A5",
+			"A6",   "A7",   "A8",   "A9",
+			"A10",  "A11",  "A12",  "A13",
+			"A14",  "A15",  "SAR", "EXCCAUSE"
+		};
+		static const char *edesc[] = {
+			"IllegalInstruction", "Syscall", "InstructionFetchError", "LoadStoreError",
+			"Level1Interrupt", "Alloca", "IntegerDivideByZero", "PCValue",
+			"Privileged", "LoadStoreAlignment", "res", "res",
+			"InstrPDAddrError", "LoadStorePIFDataError", "InstrPIFAddrError", "LoadStorePIFAddrError",
+			"InstTLBMiss", "InstTLBMultiHit", "InstFetchPrivilege", "res",
+			"InstrFetchProhibited", "res", "res", "res",
+			"LoadStoreTLBMiss", "LoadStoreTLBMultihit", "LoadStorePrivilege", "res",
+			"LoadProhibited", "StoreProhibited",
+		};
+
+		void *i_lr = (void *)exc_frame->a0;
+		void *i_pc = (void *)exc_frame->pc;
+		void *i_sp = (void *)exc_frame->a1;
+
+		void *o_pc;
+		void *o_sp;
+
+		auto panic_fmt_buffer = buffer.log_scratch_buffer();
+		size_t panic_string_size = 0;
+
+#define PANIC(fmt, ...) {\
+		ets_printf(fmt, ##__VA_ARGS__); \
+		panic_fmt_buffer = buffer.log_scratch_buffer(); \
+		if (panic_fmt_buffer.second > 0) { \
+			panic_string_size = snprintf(panic_fmt_buffer.first, panic_fmt_buffer.second, fmt, ##__VA_ARGS__); \
+			if (buffer.write_log(panic_fmt_buffer.first, panic_string_size + 1) == buffer.WriteBufferFull) return; \
+		}}
+
+		if (wdt)
+			PANIC("Task watchdog got triggered.\n");
+
+
+#ifdef CONFIG_FREERTOS_WATCHPOINT_END_OF_STACK
+		if (exc_frame->exccause == 1) {
+			uint32_t reason = soc_debug_reason();
+
+			if (reason & XCHAL_DEBUGCAUSE_DBREAK_MASK) {
+				char name[configMAX_TASK_NAME_LEN];
+
+				strncpy(name, pcTaskGetTaskName(NULL), configMAX_TASK_NAME_LEN);
+				PANIC("Stack canary watchpoint triggered (%s)\n", name);
+			}
+		} else
+#endif
+		{
+			const char *reason = exc_frame->exccause < 30 ? edesc[exc_frame->exccause] : "unknown";
+			const uint32_t *regs = (const uint32_t *)exc_frame;
+
+			PANIC("Guru Meditation Error: Core  0 panic'ed (%s). Exception was unhandled.\n", reason);
+			PANIC("Core 0 register dump:\n");
+
+			for (int i = 0; i < 20; i += 4) {
+				for (int j = 0; j < 4; j++) {
+					PANIC("%-8s: 0x%08x  ", sdesc[i + j], regs[i + j + 1]);
+				}
+				PANIC("\n");
+			}
+		}
+
+		PANIC("\nBacktrace: %p:%p ", i_pc, i_sp);
+		while(xt_retaddr_callee(i_pc, i_sp, i_lr, &o_pc, &o_sp)) {
+			PANIC("%p:%p ", o_pc, o_sp);
+			i_pc = o_pc;
+			i_sp = o_sp;
+		}
+		PANIC("\n");
+	}
+#undef PANIC
+}
