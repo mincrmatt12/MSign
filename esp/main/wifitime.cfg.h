1a8aff97ab331fcf54b6845cbf89b4e29895396f
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2024-08-30 01:36:02 -0400

    Remove remnant of old wifi config system
    no longer defined

--- esp/main/wifitime.cfg.h
+++ esp/main/wifitime.cfg.h
@@ -1,9 +1,6 @@
 #include "config.h"
 
 namespace wifi {
-	//cfg: receives .wifi.conn.$field
-	void receive_config(const char *field, const char *value);
-
 	//!cfg: holds .time.zone, default "EST5EDT,M3.2.0/2:00:00,M11.1.0/2:00:00"
 	extern config::string_t time_zone_str;
 
